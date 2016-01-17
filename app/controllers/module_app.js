2c32e7f2482af7aef826a2b5503fc9c0fccc8187
dwikuntobayu <dwikunto.bayu@kiranatama.com>
2016-01-18 01:44:33 +0700

    - .controller : -- can have multiple method -- can inherit in same scope - .directive, can process from view with link

--- app/controllers/module_app.js
+++ app/controllers/module_app.js
@@ -1,5 +1,10 @@
 // setup module with name 'module_app'
 var app = angular.module('module_app', []);
+
+// =========
+// Implement of Controller
+// =========
+// https://docs.angularjs.org/guide/controller
 // controller for hold a data user
 app.controller('ctrl_user', function($scope) {
   $scope.full_name = "M Dwikuntobayu";
@@ -9,11 +14,77 @@ app.controller('ctrl_user', function($scope) {
 app.controller('ctrl_scores', function($scope) {
   $scope.scores = [32, 54, 66, 72, 88, 102];
 });
+// controller with method
+app.controller('process_evl', function($scope) {
+  $scope.name = 'ratata';
+  $scope.to_gladius = function () {
+    $scope.name = "sharp";
+  };
+  $scope.to_golem = function() {
+    $scope.name = "strong";
+  };
+});
+// controller with method that have parameter
+app.controller('process_bind', function($scope) {
+  $scope.base = "flame rock";
+  $scope.name = "gerad";
+  $scope.combine_with = function(poke) {
+    $scope.name = poke;
+  };
+});
+// test inheritance scope base on controller
+app.controller('parent_controller', function($scope) {
+  $scope.time_of_day = 'morning';
+  $scope.temperature = '25C';
+});
+app.controller('child_controller', function($scope) {
+  $scope.time_of_day = 'noon';
+});
+app.controller('grandchild_controller', function($scope) {
+  $scope.temperature = '35C';
+});
+
+// ===========
+// // Implement of Directive
+// ===========
+// https://docs.angularjs.org/guide/directive
 // http://stackoverflow.com/questions/20018507/angularjs-what-is-the-need-of-the-directives-link-function-when-we-already-had
-// directive use for create template
-// name directive cannot sparate with dash or underscore
+// directive are markers for create attribute, element name, comment or css
+// name custom directive cannot sparate with dash or underscore
 app.directive("helloearth", function() {
   return {
-    template : "evl is take time and slowly like river"
+    // if set replace to true, make sure template block with html tag
+    replace: 'true',
+    template : "<p>evl is take time and slowly like river</p>"
+  };
+});
+// combination controller and custom directive
+app.controller('human', function($scope) {
+  $scope.identity = {
+    name: 'M Dwikuntobayu',
+    age: '26'
+  };
+})
+.directive('mdwikuntobayu', function() {
+  return {
+    template: 'Name : {{ identity.name }} <br /> Age : {{ identity.age }}'
+  };
+});
+// use link at directive for process scope controller
+// this will replace background collor from cyan to grey when click label
+app.directive('changecolor', function() {
+  return {
+    template: '<p style="background-color: {{ color }}">Yo what up, click me</p>',
+    link: function(scope, elem, attrs) {
+      elem.css('background-color', 'cyan');
+      elem.bind('click', function() {
+        scope.$apply(function() {
+          scope.color = "grey";
+        });
+      });
+      elem.bind('mouseover', function() {
+        elem.css('cursor', 'pointer');
+      });
+    }
   };
 });
