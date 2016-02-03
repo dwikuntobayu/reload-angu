b1718fe980fe6022796e5ee431d7540c301ace76
dwikuntobayu <dwikunto.bayu@kiranatama.com>
2016-02-03 09:17:32 +0700

    add api for show and update

--- app/controllers/module_crudapi.js
+++ app/controllers/module_crudapi.js
@@ -1,23 +1,19 @@
 var app = angular.module('module_crudapi', ['ngResource']);
 
+//create services for access api with resource
 app.factory('Bycycle', function($resource) {
   //error server "\xA4" ASCII-8BIT to UTF-8
   //fails convert from ascii to utf
   //use window.btoa() , this method will Creates a base-64 encoded ASCII string from a "string" of binary data
   //because in api server only know this encode (if send via header)
   var auth = window.btoa("dwikuntobayu" + ':' + "12345678");
+  var base_url = 'http://localhost:3003';
 
   return $resource('', {}, {
     'index' : {
       method : 'GET',
-      url : 'http://localhost:3003/api/v1/bycycles',
-      isArray : false
-    },
-    'save' : {
-      method : 'POST',
-      url : 'http://localhost:3003/api/v1/bycycles',
+      url : base_url + '/api/v1/bycycles',
       isArray : false,
-      // params: {username:'dwikuntobayu', password:'12345678'},
       headers : {
         'Authorization' : 'Basic ' + auth,
         'Accept' : 'application/json',
@@ -25,10 +21,61 @@ app.factory('Bycycle', function($resource) {
         'Access-Control-Allow-Headers' : 'Origin, X-Requested-With, Content-Type, Accept'
       }
     },
+    'show' : {
+      method : 'GET',
+      url : base_url + '/api/v1/bycycles/:id',
+      isArray : false,
+      headers : {
+        'Authorization' : 'Basic ' + auth,
+        'Accept' : 'application/json',
+        'Content-Type' : 'application/json; charset=UTF-8',
+        'Access-Control-Allow-Headers' : 'Origin, X-Requested-With, Content-Type, Accept'
+      },
+      params : {
+        id : '@id'
+      }
+    },
+    'save' : {
+      method : 'POST',
+      url : base_url + '/api/v1/bycycles',
+      isArray : false,
+      headers : {
+        'Authorization' : 'Basic ' + auth,
+        'Accept' : 'application/json',
+        'Content-Type' : 'application/json; charset=UTF-8',
+        'Access-Control-Allow-Headers' : 'Origin, X-Requested-With, Content-Type, Accept'
+      },
+      params : {
+        name : '@name',
+        series : '@series'
+      }
+    },
+    'update' : {
+      method : 'PUT',
+      url : base_url + '/api/v1/bycycles/:id',
+      isArray : false,
+      headers : {
+        'Authorization' : 'Basic ' + auth,
+        'Accept' : 'application/json',
+        'Content-Type' : 'application/json; charset=UTF-8',
+        'Access-Control-Allow-Headers' : 'Origin, X-Requested-With, Content-Type, Accept'
+      },
+      params : {
+        id : '@id',
+        name : '@name',
+        series : '@series'
+      }
+    },
     'destroy' : {
       method : 'DELETE',
-      url : 'http://localhost:3003/api/v1/bycycles/:id',
+      url : base_url + '/api/v1/bycycles/:id',
       isArray : false,
+      headers : {
+        'Authorization' : 'Basic ' + auth,
+        'Accept' : 'application/json',
+        'Content-Type' : 'application/json; charset=UTF-8',
+        'Access-Control-Allow-Headers' : 'Origin, X-Requested-With, Content-Type, Accept'
+      },
       params : {
         id : '@id'
       }
@@ -39,7 +86,17 @@ app.factory('Bycycle', function($resource) {
 var app_bycycle = angular.module('bycycle_module', ['module_crudapi']);
 
 app_bycycle.controller('bycycle_controller', function($scope, Bycycle) {
-  $scope.bycycles = Bycycle.save();
+  // $scope.bycycles = Bycycle.save({name: "Recon 1", series: "xc-rc1"});
+  $scope.bycycles = Bycycle.index();
+  // $scope.bycycles = Bycycle.show({id: 1});
+  // $scope.bycycles = Bycycle.update({id: 9, name: "Recon 2", series: "xc-rc2"});
+  // $scope.bycycle = Bycycle.destroy({id: 9});
+});
+
+app_bycycle.directive('showbycycle', function() {
+  return {
+    template: 'Name : {{ bycycles.bycyle.name }} <br /> Series : {{ bycycles.bycycle.series }}'
+  };
 });
 
 //
